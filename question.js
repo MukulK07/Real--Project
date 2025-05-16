287aa41f8e021674d492722519a9bf2ce2091602
MukulK07 <mukulkhatri01234@gmail.com>
2025-05-16 22:15:45 +0530

    index.html

---
+++ question.js
@@ -0,0 +1,104 @@
+let questions = [
+  {
+    numb: 1,
+    question: "Which of the following is true for variable names in Python?",
+    answer: "unlimited length",
+    options: [
+      "underscore and ampersand are the only two special characters allowed",
+      "unlimited length",
+      "all private members must have leading and trailing underscores",
+      "none of the mentioned",
+    ],
+  },
+  {
+    numb: 2,
+    question: "Which of the following is not a type of database?",
+    answer: "Decentralized",
+    options: ["Hierarchical", "Network", "Distributed", "Decentralized"],
+  },
+  {
+    numb: 3,
+    question:
+      "Suppose you knows Selection sort, Insertion sort & Quick sort which one he wil used for best time complexity ?",
+    answer: "Quick sort",
+    options: [
+      "Selection sort",
+      "Insertion sort",
+      "Quick sort",
+      "All of the above",
+    ],
+  },
+  {
+    numb: 4,
+    question:
+      "Which of the following is known as a set of entities of the same type that share same properties, or attributes?",
+    answer: "Entity set",
+    options: ["Relation set", "Tuples", "Entity set", "Entity Relation model"],
+  },
+  {
+    numb: 5,
+    question: "Which command is used to remove a relation from an SQL ?",
+    answer: "Drop table",
+    options: ["Drop table", "Delete", "Purge", "Remove"],
+  },
+  {
+    numb: 6,
+    question: "Please, Rate this Portal!",
+    answer: "None",
+    options: ["Good", "Need some improvement", "Not good", "None"],
+  },
+  {
+    numb: 7,
+    question: "What comes after 'A' in the alphabet?",
+    answer: "B",
+    options: ["C", "D", "B", "E"],
+  },
+  {
+    numb: 8,
+    question: "What is 1 + 2?",
+    answer: "3",
+    options: ["2", "3", "4", "5"],
+  },
+  {
+    numb: 9,
+    question: "What is 2 * 2?",
+    answer: "4",
+    options: ["2", "4", "6", "8"],
+  },
+  {
+    numb: 10,
+    question: "Which word starts with the letter 'C'?",
+    answer: "Cat",
+    options: ["Apple", "Ball", "Cat", "Dog"],
+  },
+  {
+    numb: 11,
+    question: "What is the color of the sun?",
+    answer: "Yellow",
+    options: ["Red", "Blue", "Green", "Yellow"],
+  },
+  {
+    numb: 12,
+    question: "What comes before number 5?",
+    answer: "4",
+    options: ["3", "2", "4", "6"],
+  },
+  {
+    numb: 13,
+    question: "Which of these is a fruit?",
+    answer: "Apple",
+    options: ["Car", "Apple", "Chair", "Book"],
+  },
+  {
+    numb: 14,
+    question: "Which one is an animal?",
+    answer: "Dog",
+    options: ["Pen", "Dog", "Cup", "Bag"],
+  },
+  {
+    numb: 15,
+    question: "What is the first letter of the English alphabet?",
+    answer: "A",
+    options: ["Z", "B", "C", "A"],
+  },
+];
